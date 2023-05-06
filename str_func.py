82fba36fe2ea556e96030c2a404fb2b35d4ac035
Dmitry <132492865+dmitry-sharushkin@users.noreply.github.com>
2023-05-06 19:15:37 +0300

    Update str_func.py

--- str_func.py
+++ str_func.py
@@ -1,8 +1,3 @@
 def str_func(string):
     """upper case"""
     return string.upper()
-
-
-def capitalize_word(string):
-    """:return capitalize word"""
-    return ' '.join(word.capitalize() for word in string.split())
