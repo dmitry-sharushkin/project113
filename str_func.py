5611f94e3ef77bf0fed1718b6c4f13c94f091492
dmitry-sharushkin <dimasharushkin@gmail.com>
2023-05-06 17:30:31 +0300

    add changes in str_func

--- str_func.py
+++ str_func.py
@@ -1,2 +1,7 @@
 def str_func(string):
     return string.upper()
+
+
+def capitalize_word(string):
+    """:return capitalize word"""
+    return ' '.join(word.capitalize() for word in string.split())
